af8498009cfe6fa0de40e44598bc7541039e5656
Lokesh <lokeshborole7@gmail.com>
2022-12-14 12:09:18 +0530

    form added

--- home/views.py
+++ home/views.py
@@ -4,7 +4,8 @@ from datetime import datetime
 from django.contrib import messages
 from home.models import Customer
 from django.contrib.auth import authenticate,login,logout
-
+from django.contrib.auth.forms import UserCreationForm #denis
+from .forms import CreateUserForm
 # password : kanha$2012
 # Create your views here.
 def index(request):
@@ -31,36 +32,44 @@ def logoutUser(request):
     logout(request)
     return redirect('/login')
 
-def signup(request):
-    if request.method == "POST":
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        phone = request.POST.get('phone')
-        age = request.POST.get('age')
-        password = request.POST.get('password')
-        signup = Customer(name=name, email=email, phone=phone, age=age, password=password, date=datetime.today())
-        signup.save()
-        messages.success(request, 'Customer Registered successfully')
-    return render(request,'signup.html')
+def signup(request): #denis
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form':form}
+    return render(request,'signup.html',context)
+
+
+    # if request.method == "POST":
+    #     name = request.POST.get('name')
+    #     email = request.POST.get('email')
+    #     phone = request.POST.get('phone')
+    #     age = request.POST.get('age')
+    #     password = request.POST.get('password')
+    #     signup = Customer(name=name, email=email, phone=phone, age=age, password=password, date=datetime.today())
+    #     signup.save()
+    #     messages.success(request, 'Customer Registered successfully')
 
 
 # for customer
-def loginCustomer(request):
-    if request.method=="POST":
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        # username = User.objects.get(email=email).username
-        # check if user has enetered correct Credentials
-        customer = authenticate(email=email, password=password)
-        if customer is not None:
-            login(request,customer)
-            # A backend authenticated the credentials
-            return redirect('customer.html')
-        else:
-            # No backend authenticated the credentials    
-            return render(request,'customerLogin.html')
-    return render(request,'customerLogin.html')
+# def loginCustomer(request):
+#     if request.method=="POST":
+#         email = request.POST.get('email')
+#         password = request.POST.get('password')
+#         # username = User.objects.get(email=email).username
+#         # check if user has enetered correct Credentials
+#         customer = authenticate(email=email, password=password)
+#         if customer is not None:
+#             login(request,customer)
+#             # A backend authenticated the credentials
+#             return redirect('customer.html')
+#         else:
+#             # No backend authenticated the credentials    
+#             return render(request,'customerLogin.html')
+#     return render(request,'customerLogin.html')
 
-def logoutCustomer(request):
-    logout(request)
-    return redirect('/customerLogin')
+# def logoutCustomer(request):
+#     logout(request)
+#     return redirect('/customerLogin')
